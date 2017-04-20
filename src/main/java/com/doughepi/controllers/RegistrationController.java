3c3d6ceb71f34ebe6bfdf3d8656d2d31a6c8707c
TheReichanadter <ajreicha@mtu.edu>
2017-04-20 18:47:33 -0400

    Finished javadocs for RegistrationController

--- src/main/java/com/doughepi/controllers/RegistrationController.java
+++ src/main/java/com/doughepi/controllers/RegistrationController.java
@@ -18,28 +18,58 @@ import java.util.logging.Logger;
 /**
  * Created by dough on 1/30/2017.
  */
+
+/**
+ * Michigan Technological University
+ * CS3141: Team Software Project
+ * <p>
+ * Phood
+ * <p>
+ * A website for the management of recipes.
+ * <p>
+ * The <code>HelpController</code> is responsible for routing requests to the /register url to the register templates.
+ *
+ * @author Piper Dougherty
+ * @author Adam Reichanadter
+ * @author De'Shawn Presley
+ * @author Tyler Schlomer
+ * @author Daniel Morgan
+ * @version 1.0.0-Alpha
+ * @since 4/20/2016
+ */
 @Controller
 @RequestMapping(value = "/register")
 @SessionAttributes("user")
 public class RegistrationController {
 
     @Autowired
+    private
     UserService userService;
 
     private final RegistrationValidator registrationValidator;
-    Logger logger = Logger.getLogger(getClass().getName());
+    private Logger logger = Logger.getLogger(getClass().getName());
 
+    /**
+     * @param registrationValidator Injects an instance of the RegistrationValidator by spring
+     */
     @Autowired
     public RegistrationController(RegistrationValidator registrationValidator) {
         this.registrationValidator =
                 registrationValidator;
     }
 
+    /**
+     * @return The location of the register-1 template in the /templates/registration directory.
+     */
     @RequestMapping
     public String paramRedirect() {
         return "redirect:/register?page=1";
     }
 
+    /**
+     * @param model The model injected by Spring for each page.
+     * @return The location of the register-1 template in the /templates/registration directory.
+     */
     @RequestMapping(params = "page=1")
     public String initialPage(final Model model) {
         //Insert a blank user into the model for the registration page.
@@ -48,6 +78,11 @@ public class RegistrationController {
         return "/registration/register-1";
     }
 
+    /**
+     * @param userModel Injects an instance of UserModel by spring
+     * @param bindingResult Injects an instance of BindingResult by spring
+     * @return The location of the register-2 template in the /templates/registration directory.
+     */
     @RequestMapping(params = "page=2")
     public String secondPage(
             final @ModelAttribute("user") UserModel userModel,
@@ -67,6 +102,12 @@ public class RegistrationController {
         return "/registration/register-2";
     }
 
+    /**
+     * @param userModel Injects an instance of UserModel by spring
+     * @param bindingResult Injects an instance of BindingResult by spring
+     * @param sessionStatus Injects an instance of SessionStatus by spring
+     * @return The location of the register-3 template in the /templates/registration directory.
+     */
     @RequestMapping(params = "page=3")
     public String thirdPage(
             final @ModelAttribute("user") UserModel userModel,
@@ -82,6 +123,11 @@ public class RegistrationController {
         return "/registration/register-3";
     }
 
+    /**
+     * @param userModel Injects an instance of UserModel by spring
+     * @param sessionStatus Injects an instance of SessionStats by spring
+     * @return The location of the index template in the /templates directory.
+     */
     @RequestMapping(params = "_confirm")
     public String processConfirm(
             final @ModelAttribute("user") UserModel userModel,
@@ -93,6 +139,10 @@ public class RegistrationController {
         return "index";
     }
 
+    /**
+     * @param sessionStatus Injects and instance os SessionStatus by spring
+     * @return The location of the index template in the /templates directory.
+     */
     @RequestMapping(params = "_cancel")
     public String processCancel(
             final SessionStatus sessionStatus
