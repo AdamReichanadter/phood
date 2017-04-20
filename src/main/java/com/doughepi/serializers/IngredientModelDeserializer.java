d92ad81e9f7ed0e024226c04ebfee42bb921ea49
TheReichanadter <ajreicha@mtu.edu>
2017-04-20 19:05:53 -0400

    Finished javadocs for ingredientModelDeserializer

--- src/main/java/com/doughepi/serializers/IngredientModelDeserializer.java
+++ src/main/java/com/doughepi/serializers/IngredientModelDeserializer.java
@@ -8,8 +8,36 @@ import java.lang.reflect.Type;
 /**
  * Created by ajreicha on 3/16/17.
  */
+
+/**
+ * Michigan Technological University
+ * CS3141: Team Software Project
+ * <p>
+ * Phood
+ * <p>
+ * A website for the management of recipes.
+ * <p>
+ * The <code>IngredientModelDeserializer</code> Takes in json elements and turn them into java objects
+ *
+ * @author Piper Dougherty
+ * @author Adam Reichanadter
+ * @author De'Shawn Presley
+ * @author Tyler Schlomer
+ * @author Daniel Morgan
+ * @version 1.0.0-Alpha
+ * @since 4/20/2016
+ */
 public class IngredientModelDeserializer implements JsonDeserializer<IngredientModel> {
 
+    /**
+     * @param jsonElement Json element being injected for deserialization
+     * @param type Type is the common superinterface for all types in the Java
+     * programming language. These include raw types, parameterized types,
+     * array types, type variables and primitive types.
+     * @param jsonDeserializationContext Injects as instance of JsonDeserializationContext by spring
+     * @return an IngredientModel object
+     * @throws JsonParseException
+     */
     @Override
     public IngredientModel deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
         JsonObject ingredientJson = jsonElement.getAsJsonObject();
@@ -25,6 +53,5 @@ public class IngredientModelDeserializer implements JsonDeserializer<IngredientM
         ingredientModel.setIngredientName(ingredientName);
 
         return ingredientModel;
-
     }
 }
