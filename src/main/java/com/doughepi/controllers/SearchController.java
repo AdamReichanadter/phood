9c46d793775c4169617bfb6753e9bb711cb963e5
TheReichanadter <ajreicha@mtu.edu>
2017-04-20 18:51:17 -0400

    Finished javadocs for SearchController

--- src/main/java/com/doughepi/controllers/SearchController.java
+++ src/main/java/com/doughepi/controllers/SearchController.java
@@ -11,26 +11,45 @@ import org.springframework.web.bind.annotation.RequestParam;
 /**
  * Created by pjdoughe on 3/23/17.
  */
+
+/**
+ * Michigan Technological University
+ * CS3141: Team Software Project
+ * <p>
+ * Phood
+ * <p>
+ * A website for the management of recipes.
+ * <p>
+ * The <code>HelpController</code> is responsible for routing requests to the /search url to the search template.
+ *
+ * @author Piper Dougherty
+ * @author Adam Reichanadter
+ * @author De'Shawn Presley
+ * @author Tyler Schlomer
+ * @author Daniel Morgan
+ * @version 1.0.0-Alpha
+ * @since 4/20/2016
+ */
 @Controller
 public class SearchController {
 
     @Autowired
+    private
     SearchService searchService;
 
+    /**
+     * @param model The model injected by Spring for each page.
+     * @param query Injects a search query in the form of a string
+     * @return The location of the search template in the /templates directory.
+     */
     @GetMapping(value = "/search", params = {"query"})
     public String showSearch(Model model, @RequestParam("query") String query) {
-
-
-
         SearchResults searchResults = searchService.getSearchResults(query);
         model.addAttribute("query", searchResults.getQuery());
         model.addAttribute("count", searchResults.getCount());
         model.addAttribute("duration", searchResults.getDuration());
         model.addAttribute("unit", searchResults.getUnit());
-
         model.addAttribute("results", searchResults.getResultList());
-
         return "search";
     }
-
 }
