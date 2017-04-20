f22f224ce4ccb7900fe9b77b9fae6e33fa6e6c67
dough <doughertypiper@gmail.com>
2017-04-20 18:07:17 -0400

    More documentation for Adam's reference.

--- src/main/java/com/doughepi/controllers/HelpController.java
+++ src/main/java/com/doughepi/controllers/HelpController.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2017 Piper Dougherty, Adam Reichanadter, De'Shawn Presley, Tyler Schlomer, Daniel Morgan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.doughepi.controllers;
 
 import org.springframework.stereotype.Controller;
@@ -5,7 +27,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
- * Created by pjdoughe on 4/12/17.
+ * Michigan Technological University
+ * CS3141: Team Software Project
+ * <p>
+ * Phood
+ * <p>
+ * A website for the management of recipes.
+ * <p>
+ * The <code>HelpController</code> is responsible for routing requests to the /help url to the help template.
+ *
+ * @author Piper Dougherty
+ * @author Adam Reichanadter
+ * @author De'Shawn Presley
+ * @author Tyler Schlomer
+ * @author Daniel Morgan
+ * @version 1.0.0-Alpha
+ * @since 4/20/2016
  */
 @Controller
 public class HelpController {
