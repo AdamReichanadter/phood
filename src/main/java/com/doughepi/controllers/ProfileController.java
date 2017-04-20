38f39aeeaffe97e1604173f175c720b0b8f79b76
dough <doughertypiper@gmail.com>
2017-04-20 18:20:16 -0400

    Pushing change of documentation in the profile controller.

--- src/main/java/com/doughepi/controllers/ProfileController.java
+++ src/main/java/com/doughepi/controllers/ProfileController.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2017 Piper Dougherty, Adam Reichanadter, De'Shawn Presley, Tyler Schlomer, Daniel Morgan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.doughepi.controllers;
 
 import com.doughepi.models.UserModel;
@@ -16,13 +38,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(value = "/profile", method = RequestMethod.GET)
 public class ProfileController {
 
-    @Autowired
-    private
+    private final
     UserService userService;
 
-    @Autowired
-    private
+    private final
     RecipeService recipeService;
+
+    @Autowired
+    public ProfileController(UserService userService, RecipeService recipeService) {
+        this.userService = userService;
+        this.recipeService = recipeService;
+    }
 
     /**
      * @param model The model injected by Spring for each page.
