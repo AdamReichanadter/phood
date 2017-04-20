84a448a6511adc6995346e7aa81ac0d14bced612
dough <doughertypiper@gmail.com>
2017-04-20 19:44:30 -0400

    Fixed class-level javadoc.

--- src/main/java/com/doughepi/services/UserDetailsServiceImpl.java
+++ src/main/java/com/doughepi/services/UserDetailsServiceImpl.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2017 Piper Dougherty, Adam Reichanadter, De'Shawn Presley, Tyler Schlomer, Daniel Morgan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.doughepi.services;
 
 import com.doughepi.models.UserModel;
@@ -15,7 +37,22 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
- * Created by dough on 1/30/2017.
+ * Michigan Technological University
+ * CS3141: Team Software Project
+ * <p>
+ * Phood
+ * <p>
+ * A website for the management of recipes.
+ * <p>
+ * The <code>UserDetailsServiceImpl</code> allows for Spring Security to find a user for login.
+ *
+ * @author Piper Dougherty
+ * @author Adam Reichanadter
+ * @author De'Shawn Presley
+ * @author Tyler Schlomer
+ * @author Daniel Morgan
+ * @version 1.0.0-Alpha
+ * @since 4/20/2016
  */
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService
@@ -25,14 +62,21 @@ public class UserDetailsServiceImpl implements UserDetailsService
     @Autowired
     public UserDetailsServiceImpl(UserRepository userRepository) {this.userRepository = userRepository;}
 
+    /**
+     * Find a user in the database by username.
+     *
+     * @param username The username to search for a user with.
+     * @return The user object found.
+     * @throws UsernameNotFoundException If the username was not found.
+     */
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException
-    {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserModel userModel = userRepository.findByUserUsername(username);
-        if (userModel == null)
-        {
+        if (userModel == null) {
             throw new UsernameNotFoundException("The supplied username does not exist in the database.");
         }
+
+        // Link a user's roles to the user.
         Set<GrantedAuthority> grantedAuthorities = userModel.getRoleSet().stream().map(roleModel -> new
                 SimpleGrantedAuthority(roleModel.getRoleName())).collect(Collectors.toSet());
         return new User(userModel.getUserUsername(), userModel.getUserPassword(), grantedAuthorities);
